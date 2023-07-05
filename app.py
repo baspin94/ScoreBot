a840e7ff36b05834a6f9b09fdb0cb929d4d34e12
Bianca Aspin <bianca@mj94.net>
2023-07-04 23:07:11 -0700

    Add 'echo' functionality for non-command text messages.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # imports from tutorial
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+# Adding MessageHandler and filters to imports
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 # additional imports
 import os
@@ -23,6 +24,10 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+# This function will be called to respond to regular (non-command) messages.
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)    
+
 if __name__ == '__main__':
 
     # Creating an application object
@@ -31,6 +36,11 @@ if __name__ == '__main__':
     # CommandHandler listens for commands
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
-    
+
+    # Now we'll add the echo handler here, too.
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    application.add_handler(echo_handler)
+
     # Runs the bot until you cancel out of it.
     application.run_polling()
